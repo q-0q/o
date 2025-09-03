8ff8b641d3b6aced9d2f0200a3d08a080cbaf70d
q-0q <jrwthrs@gmail.com>
2025-09-02 23:57:50 -0400

    feat: look speed

--- Scene/Component/Player/player.gd
+++ Scene/Component/Player/player.gd
@@ -1,14 +1,19 @@
 extends MeshInstance3D
 
-@export var max_speed = 1
-@export var acceleration_rate = 0.1
-@export var stop_rate = 10
+@export var max_speed = 0.05
+@export var acceleration_time = 0.2
+@export var stop_time = 0.1
 @export var input_dir_deadzone_size = 0.05
+@export var look_speed_multiplier = 2.25
+@export var look_range_radians = 0.75
 
+var look_angle_speed_curve : Curve
 var speed = 0.0
-var time_spent_accelerating = 0.0
-var time_spent_stopping = 0.0
+var look_speed = 0.0
 
+func _ready():
+	look_angle_speed_curve = load("res://Resource/Curve/player_look_angle_speed_curve.tres")
+	
 func _process(delta):
 	move(delta)
 
@@ -20,14 +25,22 @@ func move(delta):
 	if (Input.is_action_pressed("move_backward")): input2 += Vector2.DOWN
 	
 	if input2.length() > input_dir_deadzone_size:
-		speed += acceleration_rate * delta
+		speed += delta * max_speed / acceleration_time
 	else:
-		speed -= stop_rate * delta
+		speed -= delta * max_speed / stop_time
 	
 	speed = clamp(speed, 0, max_speed)
-	print(speed)
-	
-	position += $CameraHolder.position.normalized() * speed
+	var look = $CameraHolder.position.normalized()
+	var input3 = (Vector3(input2.x, 0, input2.y)).normalized()
+	var angle = look.angle_to(input3)
+	var w = angle / look_range_radians
+	if angle < look_range_radians:
+		var look_mod = look_angle_speed_curve.sample(w)
+		print(look_mod)
+		position += look * speed * look_mod
+		
+	else:
+		position += input3 * speed
 	
 
 func _on_game_cursor_computed_camera_position(position):
--- Script/Player/game_cursor.gd
+++ Script/Player/game_cursor.gd
@@ -16,9 +16,9 @@ func _input(event):
 	if event is not InputEventMouseMotion: return
 	var viewport_size = get_viewport().get_visible_rect().size
 	var cursor_screen_normal = get_normalized_point(viewport_size, event.position)
-	var distance = min(1, cursor_screen_normal.length())
+	var distance = min(0.8, cursor_screen_normal.length())
 	var sample = pan_curve.sample(distance)
-	var camera_position_2 = cursor_screen_normal * sample * pan_distance
+	var camera_position_2 = cursor_screen_normal.normalized() * sample * pan_distance
 	var camera_position_3 = Vector3(camera_position_2.x, 0, camera_position_2.y)
 	computed_camera_position.emit(camera_position_3)
 	
